347fc03b8f4f57ca8415dc1cbb2c4eb9a24e23b0
Carl Cappellino <ccap30@gmail.com>
2025-12-03 22:14:40 -0500

    Updated environment variable handling

--- includes/connect.php
+++ includes/connect.php
@@ -9,10 +9,11 @@ $ssl_ca_path = "/etc/ssl/certs/global-bundle.pem";
 $con->ssl_set(NULL, NULL, $ssl_ca_path, NULL, NULL);
 $con->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
-$db_host = "$DB_ENDPOINT";
-$db_user = "$DB_USER";
-$db_pass = '$DB_PASS';
-$db_name = "$DB_NAME";
+$db_host = getenv('DB_ENDPOINT');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+
 if (!$con->real_connect($db_host, $db_user, $db_pass, $db_name)) {
     die(mysqli_error($con));
 }
